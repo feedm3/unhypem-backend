5184a92a4015a159bec03854f621a45b001e781b
Fabian Dietenberger <piano.fabian@t-online.de>
2016-07-12 20:30:29 +0200

    Instantiate restTemplate bean on application startup

--- src/main/java/com/codecrafters/UnhypemApplication.java
+++ src/main/java/com/codecrafters/UnhypemApplication.java
@@ -2,11 +2,18 @@ package com.codecrafters;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UnhypemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UnhypemApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
