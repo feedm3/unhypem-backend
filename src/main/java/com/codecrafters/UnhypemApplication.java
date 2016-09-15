ef53179a015d6840e7ae3cb26f0454644d97552e
Fabian Dietenberger <piano.fabian@t-online.de>
2016-09-15 22:24:47 +0200

    Print host and port after startup

--- src/main/java/com/codecrafters/UnhypemApplication.java
+++ src/main/java/com/codecrafters/UnhypemApplication.java
@@ -1,64 +1,45 @@
 package com.codecrafters;
 
-import com.codecrafters.popular.PopularSongs;
-import com.codecrafters.popular.PopularSongsRepository;
-import com.codecrafters.song.Song;
-import com.codecrafters.song.SongRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.codecrafters.api.hypem.HypemPlaylistAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.LocalDateTime;
-import java.util.*;
+import java.net.InetAddress;
 
 @SpringBootApplication
+@EnableScheduling
 public class UnhypemApplication {
 
-	@Autowired
-	private SongRepository songRepository;
-
-	@Autowired
-	private PopularSongsRepository popularSongsRepository;
+    private final Logger LOGGER = LoggerFactory.getLogger(UnhypemApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(UnhypemApplication.class, args);
 	}
 
 	@Bean
-	CommandLineRunner afterStartup() {
+	CommandLineRunner afterStartup(final Environment environment) {
 		return args -> {
-			// add some test data
-			final Map<String, String> titleArtist = new HashMap<>();
-			titleArtist.put("Mord Fustang", "Drivel");
-			titleArtist.put("Deadmau5", "Soma");
-			titleArtist.put("Goodluck", "Back in the day");
+            final String host = InetAddress.getLocalHost().getHostAddress();
+            final String port = environment.getProperty("local.server.port");
 
-			titleArtist.forEach((artist, title) -> {
-				final Song song = new Song();
-				song.setArtist(artist);
-				song.setTitle(title);
-				songRepository.save(song);
-			});
-
-			final SortedMap<Integer, Song> currentCharts = new TreeMap<>();
-			int position = 1;
-			for (final Song song : songRepository.findAll()) {
-				currentCharts.put(position, song);
-				position++;
-			}
-
-			final PopularSongs popularSongs = new PopularSongs();
-			popularSongs.setSongs(currentCharts);
-			popularSongs.setTimestamp(LocalDateTime.now());
-			popularSongsRepository.save(popularSongs);
-		};
+            LOGGER.info("Server started at {}:{}", host, port);
+        };
 	}
 
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+
+    @Bean
+    public HypemPlaylistAdapter hypemPlaylistAdapter() {
+        return new HypemPlaylistAdapter(restTemplate());
+    }
 }
