9b603a05c110d018265e5cdf59c0b7a42f453da5
Fabian Dietenberger <piano.fabian@t-online.de>
2016-09-17 18:29:39 +0200

    Test 404 when song was found

--- src/test/java/com/codecrafters/song/SongRestControllerTest.java
+++ src/test/java/com/codecrafters/song/SongRestControllerTest.java
@@ -8,9 +8,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -30,8 +31,16 @@ public class SongRestControllerTest {
         given(songRepository.findOneByHypemMediaId(TestSongBuilder.HYPEM_MEDIA_ID))
                 .willReturn(TestSongBuilder.getSong());
 
-        mvc.perform(MockMvcRequestBuilders.get("/song/" + TestSongBuilder.HYPEM_MEDIA_ID))
+        mvc.perform(get("/song/" + TestSongBuilder.HYPEM_MEDIA_ID))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.hypemMediaId").value(TestSongBuilder.HYPEM_MEDIA_ID));
+    }
+
+    @Test
+    public void get404WhenMediaCannotBeFound() throws Exception {
+        given(songRepository.findOneByHypemMediaId(any())).willReturn(null);
+
+        mvc.perform(get("/song/" + TestSongBuilder.HYPEM_MEDIA_ID))
+                .andExpect(status().isNotFound());
     }
 }
