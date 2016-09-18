4ee98d54f35c9c1c4e17770849036f13cf11bc5f
Fabian Dietenberger <piano.fabian@t-online.de>
2016-09-18 19:28:01 +0200

    Reimplement hypem playlist adapter for better readability

--- src/main/java/com/codecrafters/api/hypem/HypemPlaylistAdapter.java
+++ src/main/java/com/codecrafters/api/hypem/HypemPlaylistAdapter.java
@@ -1,10 +1,10 @@
 package com.codecrafters.api.hypem;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +13,8 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.*;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 /**
  * This class is used as adapter for playlists on hypem.
  *
@@ -20,7 +22,7 @@ import java.util.*;
  */
 public class HypemPlaylistAdapter {
 
-    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private static final String HYPEM_API_VERSION = "1.1";
 
     private static final String HYPEM_CHARTS_1_TO_20_URL = "http://hypem.com/playlist/popular/3day/json/1";
     private static final String HYPEM_CHARTS_21_TO_40_URL = "http://hypem.com/playlist/popular/3day/json/2";
@@ -41,34 +43,61 @@ public class HypemPlaylistAdapter {
      */
     public SortedMap<Integer, HypemSong> getCurrentPopularCharts() {
         final SortedMap<Integer, HypemSong> songs = new TreeMap<>();
-        songs.putAll(getSongsFromPlaylistUrl(1, HYPEM_CHARTS_1_TO_20_URL));
-        songs.putAll(getSongsFromPlaylistUrl(21, HYPEM_CHARTS_21_TO_40_URL));
-        songs.putAll(getSongsFromPlaylistUrl(41, HYPEM_CHARTS_41_TO_50_URL));
+        songs.putAll(getSongsFromPlaylistUrlWithPositionOffset(HYPEM_CHARTS_1_TO_20_URL, 1));
+        songs.putAll(getSongsFromPlaylistUrlWithPositionOffset(HYPEM_CHARTS_21_TO_40_URL, 21));
+        songs.putAll(getSongsFromPlaylistUrlWithPositionOffset(HYPEM_CHARTS_41_TO_50_URL, 41));
         return songs;
     }
 
-    private SortedMap<Integer, HypemSong> getSongsFromPlaylistUrl(final int positionOffset, final String playlistUrl) {
+    private SortedMap<Integer, HypemSong> getSongsFromPlaylistUrlWithPositionOffset(final String playlistUrl, final int positionOffset) {
+        final String playlistJson = requestPlaylistJson(playlistUrl);
+        final JsonNode playlistNode = convertJsonToJsonNode(playlistJson);
+        if (playlistNode != null) {
+            final String version = playlistNode.get("version").asText();
+            checkArgument(StringUtils.equals(version, HYPEM_API_VERSION), "Hypem API version has changed. Checkout JSON parser");
+
+            final SortedMap<Integer, HypemSong> songs = new TreeMap<>();
+
+            final Iterator<Map.Entry<String, JsonNode>> songsFields = playlistNode.fields();
+            songsFields.forEachRemaining(playlistEntry -> {
+                final Pair<Integer, HypemSong> hypemSong = getPositionAndSongFromJson(playlistEntry);
+                if (hypemSong != null) {
+                    final HypemSong song = hypemSong.getValue();
+                    final int position = positionOffset + hypemSong.getKey();
+                    songs.put(position, song);
+                }
+            });
+            return songs;
+        } else {
+            return Collections.emptySortedMap();
+        }
+    }
+
+    private Pair<Integer, HypemSong> getPositionAndSongFromJson(final Map.Entry<String, JsonNode> positionSongEntry) {
+        if (NumberUtils.isNumber(positionSongEntry.getKey())) {
+            final HypemSong hypemSong = getSongFromJson(positionSongEntry.getValue().asText());
+            final int position = Integer.parseInt(positionSongEntry.getKey());
+            return Pair.of(position, hypemSong);
+        }
+        return null;
+    }
+
+    private HypemSong getSongFromJson(final String songJson) {
+        return objectMapper.convertValue(songJson, HypemSong.class);
+    }
+
+    private String requestPlaylistJson(final String playlistUrl) {
         final RequestEntity<Void> playlistRequest = RequestEntity.get(URI.create(playlistUrl)).build();
         final ResponseEntity<String> playlistResponse = restTemplate.exchange(playlistRequest, String.class);
+        return playlistResponse.getBody();
+    }
+
+    private JsonNode convertJsonToJsonNode(final String json) {
         try {
-            final JsonNode songsNode = objectMapper.readTree(playlistResponse.getBody());
-            final Iterator<Map.Entry<String, JsonNode>> songsFields = songsNode.fields();
-            final SortedMap<Integer, HypemSong> songs = new TreeMap<>();
-            while (songsFields.hasNext()) {
-                final Map.Entry<String, JsonNode> songField = songsFields.next();
-                try {
-                    final HypemSong song = objectMapper.treeToValue(songField.getValue(), HypemSong.class);
-                    final int position = positionOffset + Integer.parseInt(songField.getKey());
-                    songs.put(position, song);
-                } catch (JsonProcessingException e) {
-                    // the json root node contains some meta data which cannot be parsed to a song
-                    logger.debug("Cannot convert json node to song", e);
-                }
-            }
-            return songs;
-        } catch (IOException e) {
+            return objectMapper.readTree(json);
+        } catch (final IOException e) {
             e.printStackTrace();
-            return Collections.emptySortedMap();
+            return null;
         }
     }
 }
