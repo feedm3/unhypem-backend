ccf49bbcfcae3ef50a6b8ec5c053db6116222f90
Fabian Dietenberger <fabian.dietenberger@sinnerschrader.com>
2017-03-02 17:25:16 +0100

    Put startup runner into own class for better structure

---
+++ src/main/java/com/codecrafters/StartupRunner.java
@@ -0,0 +1,30 @@
+package com.codecrafters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+
+
+@Component
+public class StartupRunner implements CommandLineRunner {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(UnhypemApplication.class);
+
+    private final Environment environment;
+
+    public StartupRunner(final Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public void run(final String... args) throws Exception {
+        final String host = InetAddress.getLocalHost().getHostAddress();
+        final String port = environment.getProperty("local.server.port");
+
+        LOGGER.info("Server started at {}:{}", host, port);
+    }
+}
--- src/main/java/com/codecrafters/UnhypemApplication.java
+++ src/main/java/com/codecrafters/UnhypemApplication.java
@@ -2,17 +2,11 @@ package com.codecrafters;
 
 import com.codecrafters.api.hypem.HypemApi;
 import com.codecrafters.api.hypem.HypemConfiguration;
-import com.codecrafters.popular.PopularSongsRepository;
-import com.codecrafters.song.SongRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
@@ -20,27 +14,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import java.net.InetAddress;
-
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
 @EnableJpaAuditing
 public class UnhypemApplication {
 
-    private final Logger LOGGER = LoggerFactory.getLogger(UnhypemApplication.class);
-
     public static void main(final String[] args) {
         SpringApplication.run(UnhypemApplication.class, args);
-    }
-
-    @Bean
-    @Autowired
-    CommandLineRunner afterStartup(final Environment environment, final PopularSongsRepository popularSongsRepository, final SongRepository songRepository) {
-        return (String... args) -> {
-            final String host = InetAddress.getLocalHost().getHostAddress();
-            final String port = environment.getProperty("local.server.port");
-        };
     }
 
     @Bean
